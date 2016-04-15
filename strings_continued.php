1a83e1d8f06ac0d979afa83699e9c3f4a72b32e8
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-15 09:47:57 -0500

    Strings exercise.

---
+++ strings_continued.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "We don't need no education\nWe don't need no thought control\n";
+
+$stage = <<<ACT
+All the world's a stage,
+And all the men and women merely players:
+They have their exits and their entrances;
+ACT;
+
+echo $stage .PHP_EOL;
+
+$firstName = 'Jose';
+$lastName = 'Blow';
+$address = '123 Any Street';
+
+echo "$lastName, $firstName, $address\n";
+echo "$firstName$lastName$address\n";
+echo "$firstName $lastName\n$address\n";
+
