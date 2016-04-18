746559326fbcf5d025841cb00e7ff9b74aed9a68
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-18 13:31:03 -0500

    Added comments to code.

--- for.php
+++ for.php
@@ -4,8 +4,9 @@ $start = $argv[1];
 $end = $argv[2];
 $increment = 1;
 
+//checks to make sure that input is a number. 
 if (is_numeric($argv[1]) == false || is_numeric($argv[2]) == false) {
-	echo "Please enter whole numbers\n";
+	echo "Please enter whole numbers!\n";
 }
 
 //if user enters value for increment, use that as the value to increment by. 
@@ -13,7 +14,7 @@ if ($argc == 4){
 	$increment = $argv[3];
 }
 
-
+//Prints out each number given user parameters. 
 for ($i = $start; $i <= $end; $i += $increment) {
     echo "{$i}\n";
 }
