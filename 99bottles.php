637e877e4827940d7298b326928e107694cbc1a6
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-18 11:49:00 -0500

    Added comments.

--- 99bottles.php
+++ 99bottles.php
@@ -3,17 +3,26 @@
 $bottles = 99;
 $less_bottles = $bottles - 1;
 
+echo "So what do you want to drink today?\n";
+
+$drink = trim(fgets(STDIN));
+usleep(2000000);
+
+echo "That reminds me of a song...\n";
+usleep(2000000);
+
 while ($bottles > 0){
 	if ($bottles > 1){
-		echo "{$bottles} bottles of beer on the wall, {$bottles} bottles of beer. Take one down and pass it around. {$less_bottles} bottles of beer on the wall.\n";
+		echo "{$bottles} bottles of {$drink} on the wall, {$bottles} bottles of {$drink}. Take one down and pass it around. {$less_bottles} bottles of {$drink} on the wall.\n";
 		$bottles--;
 		$less_bottles = $bottles - 1;
+		usleep(2000000);
 	} else if ($bottles = 1){
-		echo "{$bottles} bottle of beer on the wall. {$bottles} bottle of beer.  Take one down and pass it around, no more bottles of beer on the wall.\n";
+		echo "{$bottles} bottle of {$drink} on the wall. {$bottles} bottle of {$drink}.  Take one down and pass it around, no more bottles of {$drink} on the wall.\n";
 		$bottles--;
+		usleep(2000000);
 	}
 }
 
-echo "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.\n
-	";
+echo "No more bottles of {$drink} on the wall, no more bottles of {$drink}. Go to the store and buy some more, 99 bottles of {$drink} on the wall.\n";
 
