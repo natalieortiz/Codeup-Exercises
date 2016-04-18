1a701a492966c7023a650da222d849bccaa38a6d
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-18 15:23:38 -0500

    Finished foreach exercise by printing out all the items in the array according to data type.

--- foreach.php
+++ foreach.php
@@ -2,29 +2,39 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-foreach ($things as $item) {
-	echo gettype($item) . PHP_EOL;
+echo "Prints type using gettype()." . PHP_EOL;
+foreach ($things as $thing) {
+	echo gettype($thing) . PHP_EOL;
 }
 
-// foreach ($things as $item) {
-// 	if (is_int($item)){
-// 		echo "{$item} is a number.\n";
-// 	} else if (is_float($item)){
-// 		echo "{$item} is a floating number.\n";
-// 	} else if (is_bool($item)) {
-// 		echo "{$item} is a boolean.\n";
-// 	} else if (is_array($item)) {
-// 		echo print_r($item) . " is an array.\n";
-// 	} else if (is_null($item)) {
-// 		echo "{$item} is a null.\n";
-// 	} else if (is_string($item)) {
-// 		echo "{$item} is a string.\n";
-// 	}
-// }	
+echo PHP_EOL . "Goes through if and else ifs to figure out what type each one is." . PHP_EOL;
 
-foreach ($things as $item) {
-	if (is_scalar($item)){
-		echo "{$item} is a scalar.\n";
+foreach ($things as $thing) {
+	if (is_int($thing)){
+		echo "{$thing} is a number.\n";
+	} else if (is_float($thing)){
+		echo "{$thing} is a floating number.\n";
+	} else if (is_bool($thing)) {
+		echo "{$thing} is a boolean.\n";
+	} else if (is_array($thing)) {
+		echo print_r($thing) . " is an array.\n";
+	} else if (is_null($thing)) {
+		echo "{$thing} is a null.\n";
+	} else if (is_string($thing)) {
+		echo "{$thing} is a string.\n";
 	}
 }
+	
+echo PHP_EOL . "Prints out all the items in the array." . PHP_EOL;
 
+foreach ($things as $thing) {
+	if (is_scalar($thing)){
+		echo $thing .PHP_EOL;
+	} else if (is_array($thing)) {
+		print_r($thing) . PHP_EOL;
+	} else if (is_bool($thing)) {
+		var_dump($thing) .PHP_EOL;
+	} else if (is_null($thing)) {
+		echo $thing . PHP_EOL;
+	}
+}
