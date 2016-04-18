10f07481a77e885fc8f1c76e0206805240c34109
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-18 12:57:03 -0500

    Defaults increment to 1 if none is given.  Gives error message if non-numeric values are entered.

--- for.php
+++ for.php
@@ -2,8 +2,16 @@
 
 $start = $argv[1];
 $end = $argv[2];
-$increment = $argv[3];
+$increment = 1;
 
+if (is_numeric($argv[1]) == false || is_numeric($argv[2]) == false) {
+	echo "Please enter whole numbers\n";
+}
+
+//if user enters value for increment, use that as the value to increment by. 
+if ($argc == 4){
+	$increment = $argv[3];
+}
 
 
 for ($i = $start; $i <= $end; $i += $increment) {
