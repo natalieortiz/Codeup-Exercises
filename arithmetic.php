097bc2849eb16b980c44748b9f5006349220615a
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-19 15:48:35 -0500

    Created a function called throwErrorMessage that returns an error message to the user.

--- arithmetic.php
+++ arithmetic.php
@@ -3,54 +3,59 @@
 function add($a, $b)
 {
 	if (is_numeric($a) && is_numeric($b)){
-	    return $a + $b . PHP_EOL;	
+	    return $a + $b;	
 	}
 	else {
-		return "Both values must be numeric!\n";
+		return throwErrorMessage($a , $b);
 	}
 }
 
 function subtract($a, $b)
 {
 	if (is_numeric($a) && is_numeric($b)){
-	    return $a - $b . PHP_EOL; 	
+	    return $a - $b; 	
 	} else {
-		return "Both values must be numeric!\n";
+		return throwErrorMessage($a , $b);
 	}
 }
 
 function multiply($a, $b)
 {	
 	if (is_numeric($a) && is_numeric($b)) {
-		return $a * $b . PHP_EOL; 	
+		return $a * $b; 	
 	} else {
-		return "Both values must be numeric!\n";
+		return throwErrorMessage($a , $b);
 	}
 }
 
 function divide($a, $b)
 {
 	if ($b == 0){
-		return "Please do not divide by zero.\n";
+		return "Please do not divide by zero.";
 	} else if (is_numeric($a) && is_numeric($b)) {
-		return $a / $b . PHP_EOL; 	
+		return $a / $b; 	
 	} else {
-		return "Both values must be numeric!";
+		return throwErrorMessage($a , $b);
 	}
 }
 
 function modulus($a, $b)
 {
     if (is_numeric($a) && is_numeric($b)) {
-		return $a % $b . PHP_EOL; 	
+		return $a % $b; 	
 	} else {
-		return "Both values must be numeric!\n";
+		return throwErrorMessage($a , $b);
 	}
 }
 
+function throwErrorMessage ($a , $b){
+	return "{$a} and {$b} must be numeric!";
+}
 
-echo add(12,3);
-echo subtract('hi',3);
-echo multiply(12,3);
-echo divide(12,0);
-echo modulus(12,3);
+
+echo add('twelve',3) . PHP_EOL;
+echo subtract('hi',3) . PHP_EOL;
+echo multiply(12,3) . PHP_EOL;
+echo divide(12,0) . PHP_EOL;
+echo modulus(12,3) . PHP_EOL;
+
