e01d2ab0fb35126db99c03528f724575d39b9c3f
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-19 14:25:32 -0500

    Added function that finds modulus of two numbers.

--- arithmetic.php
+++ arithmetic.php
@@ -20,7 +20,13 @@ function divide($a, $b)
     echo $a / $b . PHP_EOL; 
 }
 
+function modulus($a, $b)
+{
+    echo $a % $b . PHP_EOL; 
+}
+
 add(15,15);
 subtract(15,15);
 multiply(15,15);
 divide(15,15);
+modulus(15,15);
