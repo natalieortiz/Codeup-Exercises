f79c9f2bf0c9094e2a1ab3e2cd90d21f0603d330
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-20 14:15:09 -0500

    Compares values in two arrays and sees how many they have in common.

--- search-arrays.php
+++ search-arrays.php
@@ -14,5 +14,17 @@ function searchArray($name, $array)
 	
 }
 
+function compareArrays($array1, $array2)
+{	
+	$count = 0;
+	foreach($array1 as $name){
+		if (array_search($name, $array2) !== false){
+			$count++;
+		}	
+	}
+	return $count; 
+}
+
 echo searchArray('Tina',$names) . PHP_EOL;
 echo searchArray('Bob',$names) . PHP_EOL;
+echo compareArrays($names, $compare) . PHP_EOL;
